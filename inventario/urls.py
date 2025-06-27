31545b709c64d4706eb349d5c6821847a74a6d31
Alexander <aaalexander2123@gmail.com>
2025-06-26 20:49:34 -0500

    prueba 16

--- inventario/urls.py
+++ inventario/urls.py
@@ -22,6 +22,6 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index, name='inicio')
+    path('', views.index, name='inicio')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
