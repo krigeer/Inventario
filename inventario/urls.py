18bbaf8cd4cce9389b499137d1ef888c13e39865
Alexander <aaalexander2123@gmail.com>
2025-06-26 21:04:04 -0500

    prueba 20

--- inventario/urls.py
+++ inventario/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('prueba.urls'))
+    path('', include('prueba.urls'), name='inicio'),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
