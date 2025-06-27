ac779d7c88ec26ad0aa99b4422982fa1630268e7
Alexander <aaalexander2123@gmail.com>
2025-06-26 19:39:34 -0500

    prueba de despliegue 2

--- inventario/settings.py
+++ inventario/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'inventario.wsgi.app'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'inventariosena',
+        'USER': 'root',
+        'PASSWORD': 'Al3xander2123**',
+        'HOST': 'localhost',
+        'PORT': '443'
     }
 }
 
